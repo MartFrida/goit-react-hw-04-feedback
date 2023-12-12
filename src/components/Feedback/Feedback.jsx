6b4c0f909c010d877b75b3acb08fc8d7aba26acf
Mary <32392607+MartFrida@users.noreply.github.com>
2023-12-12 15:39:01 +0100

    refactor hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -10,7 +10,6 @@ export const Feedback = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  // const [grade, setGrade] = useState(0);
 
   const mapState = {
     bad: setBad,
@@ -19,9 +18,6 @@ export const Feedback = () => {
   }
 
   const onLeaveFeedback = (grade) => {
-    // setGrade(prev => prev + 1)
-    // mapState.good(prev => prev + 1)
-
     mapState[grade](prev => prev + 1)
   }
 
@@ -44,45 +40,3 @@ export const Feedback = () => {
     </StyledFeedback>
   )
 }
-
-
-
-
-
-
-
-// export class Feedback extends React.Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0
-//   }
-
-//   onLeaveFeedback = (grade) => {
-//     this.setState(prevState => ({ [grade]: prevState[grade] + 1 }))
-//   }
-
-//   countTotalFeedback() {
-//     const { good, neutral, bad } = this.state;
-//     return good + neutral + bad;
-//   }
-//   countPositiveFeedbackPercentage(good, total) {
-//     return (good * 100 / total).toFixed(2);
-//   }
-
-//   render() {
-//     const { good, neutral, bad } = this.state
-//     const total = this.countTotalFeedback()
-//     return (
-//       <StyledFeedback>
-//         <Section title='Please leave feedback'>
-//           <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} />
-//           {total ?
-//             <Statistics good={good} neutral={neutral} bad={bad} total={total} countTotalFeedback={this.countTotalFeedback()} countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage(good, total)}
-//             /> : <Notification message="There is no feedback" />
-//           }
-//         </Section>
-//       </StyledFeedback>
-//     )
-//   }
-// }
